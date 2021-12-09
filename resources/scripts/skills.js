efdca7113ccc813c5b8ea1ca0dae10a39eac0d7e
Voodoo-Dolls <mrmichaeldam@gmail.com>
2021-12-08 23:08:50 -0700

    video test

--- resources/scripts/skills.js
+++ resources/scripts/skills.js
@@ -1,53 +1,46 @@
+
 function levelFive(){
-    let x = document.querySelectorAll('._5, ._10, ._15, ._20, ._25');
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
+    reset();
     let y = document.querySelectorAll('._5');
-    for (i=0 ; i < x.length; i++){
+    for (i=0 ; i < y.length; i++){
         y[i].style.display ='block';
     }
 }
 function levelTen(){
-    let x = document.querySelectorAll('._5, ._10, ._15, ._20, ._25');
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
+    reset();
     let y = document.querySelectorAll('._10');
-    for (i=0 ; i < x.length; i++){
+    for (i=0 ; i < y.length; i++){
         y[i].style.display ='block';
     }
 }
 
 function levelFifteen(){
-    let x = document.querySelectorAll('._5, ._10, ._15, ._20, ._25');
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
+    reset();
     let y = document.querySelectorAll('._15');
-    for (i=0 ; i < x.length; i++){
+    for (i=0 ; i < y.length; i++){
         y[i].style.display ='block';
     }
 }
 
 function levelTwenty(){
-    let x = document.querySelectorAll('._5, ._10, ._15, ._20, ._25');
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
+    reset();
     let y = document.querySelectorAll('._20');
-    for (i=0 ; i < x.length; i++){
+    for (i=0 ; i < y.length; i++){
         y[i].style.display ='block';
     }
 }
 
 function levelTwentyfive(){
+    reset();
+    let y = document.querySelectorAll('._25');
+    for (i=0 ; i < y.length; i++){
+        y[i].style.display ='block';
+    }
+}
+
+function reset(){
     let x = document.querySelectorAll('._5, ._10, ._15, ._20, ._25');
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
-    }
-    let y = document.querySelectorAll('._25');
-    for (i=0 ; i < x.length; i++){
-        y[i].style.display ='block';
     }
 }
